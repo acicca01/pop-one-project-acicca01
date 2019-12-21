118c6cfedfe5838eeab63184aa457eda308907c7
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2019-12-21 12:05:23 +0000

    Implementation start of find_best_cycle()
    Adds a parameter 'doit' to the original skeleton.
    This is to automate execution of N road map reconfiguration
    where each attempt is a group of 10,000 location shifts/swaps

--- cities.py
+++ cities.py
@@ -1,5 +1,6 @@
-from math import sqrt
+from math import sqrt, ceil
 from copy import copy
+from random import random
 
 
 # TODO create all-combo distances Matrix
@@ -40,52 +41,61 @@ def compute_total_distance(road_map):
     else:
         for i in range(roadmap_length):
             tot += sqrt((road_map[(i + 1) % roadmap_length][2] - road_map[i % roadmap_length][2]) ** 2 + (
-                        road_map[(i + 1) % roadmap_length][3] - road_map[i % roadmap_length][3]) ** 2)
+                    road_map[(i + 1) % roadmap_length][3] - road_map[i % roadmap_length][3]) ** 2)
 
     return tot
 
 
 def swap_cities(road_map, index1, index2):
+    """Creates a copy of road_map and applies the swap to the copy not the original"""
     # TODO check how to destroy shallow copy
-    """
-    Take the city at location `index` in the `road_map`, and the 
-    city at location `index2`, swap their positions in the `road_map`, 
-    compute the new total distance, and return the tuple 
-
-        (new_road_map, new_total_distance)
-
-    Allow for the possibility that `index1=index2`,
-    and handle this case correctly.
-    """
-    if max(index1,index2) >= len(road_map):
-        raise Exception(f"At least one of your indexes exceeds {len(road_map)-1}. What were you thinking ?")
+    if max(index1, index2) >= len(road_map):
+        raise Exception(f"At least one of your indexes exceeds {len(road_map) - 1}. What were you thinking ?")
     new_road_map = copy(road_map)
-    new_road_map[index1] , new_road_map[index2] = road_map[index2] , road_map[index1]
+    new_road_map[index1], new_road_map[index2] = road_map[index2], road_map[index1]
 
-
-    return (new_road_map,compute_total_distance( new_road_map))
+    return (new_road_map, compute_total_distance(new_road_map))
 
 
 def shift_cities(road_map):
-    """
-    For every index i in the `road_map`, the city at the position i moves
-    to the position i+1. The city at the last position moves to the position
-    0. Return the new road map. 
-    """
+    # modifies map in place
     l = len(road_map)
     assert (l > 1), f"The map you passed to shift_cities() has {l} cities. That is not enough."
-    road_map[0] , road_map[1:] = road_map [l-1] , road_map [:(l-1)]
+    road_map[0], road_map[1:] = road_map[l - 1], road_map[:(l - 1)]
     return road_map
 
 
-def find_best_cycle(road_map):
+def find_best_cycle(road_map,doit = 3):
     """
-    Using a combination of `swap_cities` and `shift_cities`, 
-    try `10000` swaps/shifts, and each time keep the best cycle found so far. 
+    Using a combination of `swap_cities` and `shift_cities`,
+    try `10000` swaps/shifts, and each time keep the best cycle found so far.
     After `10000` swaps/shifts, return the best cycle found so far.
     Use randomly generated indices for swapping.
     """
-    pass
+    # builds a swapping tuple
+    random_list1 = []
+    random_list2 = []
+    for i in range(len(road_map)):
+        random_list1.append((ceil(len(road_map) * random())))
+        random_list2.append((ceil(len(road_map) * random())))
+    indexes = tuple(zip(random_list1,random_list2))
+    done = False
+    # initialize operation number
+    op_number = 0
+    opt_map = []
+    while not done:
+        print ("Heeeeeey")
+        op_number += 1
+        for i in range(10000):
+            if i == 1:
+                opt_map = swap_cities(road_map, *indexes)
+            else:
+                opt_map = swap_cities(opt_map, *indexes)
+        shift_cities(opt_map)
+        if op_number > doit:
+            done = True
+    return opt_map
+
 
 
 def print_map(road_map):
@@ -116,7 +126,7 @@ def print_map(road_map):
 def main():
     # TODO check number of connection == len(map)
     # TODO prompt for input file functionality
-    # print_cities(read_cities('city-data.txt',2))
+    print_cities(read_cities('city-data.txt', 2))
 
     """ 
     Reads in, and prints out, the city data, then creates the "best"
