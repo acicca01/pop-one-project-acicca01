9b3e1354c85284f61e1883bcaffb07ba47005ab9
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2020-01-01 12:01:04 +0100

    Update to visualise.py
    Grid is taking shape

--- visualise.py
+++ visualise.py
@@ -1,8 +1,23 @@
-for i in range(-1,360,1):
-    for j in range(-1,180):
-        if j%2 == 0:
-            print('-',end ='')
-        else:
-            print('|',end='')
-    print()
-    
+for i in range(-180, 181):
+    print(i, end='  ')
+print()
+
+# looping through valid coordinates range
+for j in range (-92 , 91):
+        for i in range(-182, 181):
+            if i >= -180:
+                if len(str(i)) == 4:
+                    print('  | ', end='  ')
+                if len(str(i)) == 3:
+                    print(' | ', end='  ')
+                if len(str(i)) == 2:
+                    print(' |', end='  ')
+                if len(str(i)) == 1:
+                    print('|', end='  ')
+            elif i == -182:
+                print(j)
+        print()
+        
+
+
+
