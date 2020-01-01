7a7a6b1c45c9afb5593f37c6b5900853e84a5851
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2020-01-01 18:07:40 +0100

    Update to visualise.py
    Add latitude longitude scales

--- visualise.py
+++ visualise.py
@@ -1,23 +1,33 @@
+print ('    ', end = '')
 for i in range(-180, 181):
     print(i, end='  ')
-print()
-
+print(' ')
+print('  ')
 # looping through valid coordinates range
-for j in range (-92 , 91):
-        for i in range(-182, 181):
-            if i >= -180:
-                if len(str(i)) == 4:
-                    print('  | ', end='  ')
-                if len(str(i)) == 3:
-                    print(' | ', end='  ')
-                if len(str(i)) == 2:
-                    print(' |', end='  ')
-                if len(str(i)) == 1:
-                    print('|', end='  ')
-            elif i == -182:
-                print(j)
-        print()
-        
+for j in range (-90 , -87):
+
+    for i in range(-182, 181):
+        if i >= -180:
+            #adds some padding for | | | rows
+            if i == -180:
+                print ('    ', end = '')
+            #aligns | | | rows to longitude row
+            if len(str(i)) == 4:
+                print('  | ', end='  ')
+            if len(str(i)) == 3:
+                print(' | ', end='  ')
+            if len(str(i)) == 2:
+                print(' |', end='  ')
+            if len(str(i)) == 1:
+                print('|', end='  ')
+
+        elif i == -182:
+            #adds latitude column
+            print(j , end =' ')
+            print()
+
+    print()
+
 
 
 
