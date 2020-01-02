b94604684b0a23226a0d437dad40b27ea12e7f9b
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2020-01-02 14:12:35 +0100

    Complete visualise.py
    The script takes a map of type (list of tuples)
    and prints out a grid as per specs

--- visualise.py
+++ visualise.py
@@ -1,23 +1,33 @@
-#load latitude-longitude ranges into tuple
-d = dict()
+map_dic = dict()
 latitudes = []
 longitudes = []
 #load list in dictionary
 for i in range(len(amap)):
-    d[(int(amap[i][2]),int(amap[i][3]))] = i
+    map_dic[(int(amap[i][2]),int(amap[i][3]))] = i
     latitudes.append(int(amap[i][2]))
     longitudes.append(int(amap[i][3]))
+    # load latitude-longitude ranges into tuple
     lat_range = (min(latitudes),max(latitudes)+1)
     long_range = (min(longitudes),max(longitudes)+1)
 
 print ('      ', end = '')
 for i in range(*long_range):
-    print(i, end='  ')
+    if len(str(i)) == 4:
+        print(i, end='  ')
+    elif len(str(i)) == 3:
+        print(i, end='   ')
+    elif len(str(i)) == 2:
+        print(i, end='    ')
+    elif len(str(i)) == 1:
+        print(i, end='     ')
 print()
 
 # looping through valid coordinates range
 for j in range (*lat_range):
-    print('   ', end='')
+    if len(str(j)) == 3:
+        print('   ', end='')
+    if len(str(j)) == 2:
+        print('  ', end='')
     for i in range(*long_range):
         print('  ', ' ', sep='|', end='  ')
     print()
@@ -25,8 +35,17 @@ for j in range (*lat_range):
         if i == long_range[0]:
             print(j,end='   ')
         #print(f"{'-':^6}", end='')
-        if (i , j) == (-177 , -89):
-            print('  ', ' ', sep='1', end='  ')
+
+        if (j , i) in map_dic:
+            tmp = str(map_dic[(j,i)])
+            if len(tmp) == 1:
+                print('  ', ' ', sep=tmp, end='  ')
+            if len(tmp) == 2:
+                print(' ', ' ', sep=tmp, end='  ')
+            if len(tmp) == 3:
+                print('', ' ', sep=tmp, end='  ')
+            if len(tmp) == 4:
+                print('', '', sep=tmp, end='  ')
         else:
             print(f"{'-':^6}", end='')
     print()
