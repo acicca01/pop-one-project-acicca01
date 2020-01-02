0dbe7cb08a79c2e269db9c14c1c117777458d62a
Antonello Ciccarone <acicca01@dcs.bbk.ac.uk>
2020-01-02 11:58:46 +0100

    Update to visualise.py
    Store map information into a dictionary.This is
    to allow easy retrieval of map placement
    given a city's cohordinates.
    Derive dimensions of grid from latitude/longitude
    ranges of map

--- visualise.py
+++ visualise.py
@@ -1,32 +1,47 @@
-print ('    ', end = '')
-for i in range(-180, 181):
+#load latitude-longitude ranges into tuple
+d = dict()
+latitudes = []
+longitudes = []
+#load list in dictionary
+for i in range(len(amap)):
+    d[(int(amap[i][2]),int(amap[i][3]))] = i
+    latitudes.append(int(amap[i][2]))
+    longitudes.append(int(amap[i][3]))
+    lat_range = (min(latitudes),max(latitudes)+1)
+    long_range = (min(longitudes),max(longitudes)+1)
+
+print ('      ', end = '')
+for i in range(*long_range):
     print(i, end='  ')
-print(' ')
-print('  ')
+print()
+
 # looping through valid coordinates range
-for j in range (-90 , -87):
-
-    for i in range(-182, 181):
-        if i >= -180:
-            #adds some padding for | | | rows
-            if i == -180:
-                print ('    ', end = '')
-            #aligns | | | rows to longitude row
-            if len(str(i)) == 4:
-                print('  | ', end='  ')
-            if len(str(i)) == 3:
-                print(' | ', end='  ')
-            if len(str(i)) == 2:
-                print(' |', end='  ')
-            if len(str(i)) == 1:
-                print('|', end='  ')
-
-        elif i == -182:
-            #adds latitude column
-            print(j , end =' ')
-            print()
-
+for j in range (*lat_range):
+    print('   ', end='')
+    for i in range(*long_range):
+        print('  ', ' ', sep='|', end='  ')
     print()
+    for i in range(*long_range):
+        if i == long_range[0]:
+            print(j,end='   ')
+        #print(f"{'-':^6}", end='')
+        if (i , j) == (-177 , -89):
+            print('  ', ' ', sep='1', end='  ')
+        else:
+            print(f"{'-':^6}", end='')
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
